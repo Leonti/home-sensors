fb5df452d48e2141b5132cfdba9cafad4a9a701c
Leonti Bielski <prishelec@gmail.com>
2016-07-04 12:57:19 +0000

    Add systemd service

--- README.md
+++ README.md
@@ -4,4 +4,13 @@ Reading team,humidity and co2
 Getty has to be disabled:  
 `sudo systemctl disable serial-getty@ttyAMA0.service`  
 
+http://www.raspberrypi-spy.co.uk/2015/10/how-to-autorun-a-python-script-on-boot-using-systemd/  
 
+sudo cp sensor-loop.service /lib/systemd/system/sensor-loop.service  
+
+sudo chmod 644 /lib/systemd/system/sensor-loop.service  
+
+sudo systemctl daemon-reload  
+sudo systemctl enable sensor-loop.service  
+
+sudo systemctl status sensor-loop.service  
--- loop.py
+++ loop.py
@@ -1,8 +1,24 @@
 from dht22 import dht22
 from mh_z19 import mh_z19
 
-co2 = mh_z19()
-humidity, temperature = dht22()
+import time
+import sqlite3
 
-print('Temp={0:0.1f}*  Humidity={1:0.1f}% CO2={2:d}'.format(temperature, humidity, co2))
+conn = sqlite3.connect('/home/pi/sensor-data.db')
+c = conn.cursor()
 
+c.execute("CREATE TABLE IF NOT EXISTS log (temperature DOUBLE, humidity DOUBLE, CO2 INTEGER, timestamp INTEGER)")
+
+
+try:
+  while True:
+    co2 = mh_z19()
+    humidity, temperature = dht22()
+    
+    print('Temp={0:0.1f}*  Humidity={1:0.1f}% CO2={2:d}'.format(temperature, humidity, co2))
+
+    c.execute("INSERT INTO log VALUES (?,?,?,?)", (temperature, humidity, co2, int(time.time())))
+    conn.commit()
+    time.sleep(60)
+finally:
+  conn.close()
